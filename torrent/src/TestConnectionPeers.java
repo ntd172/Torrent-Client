10b65b00c58be36b11c3b48530be2d94fe43c21d
Do Nguyen <ntd172@gmail.com>
2012-04-05 14:04:56 -0700

    fixed some directory and add new reference for bit-torrent peer wire protocol

--- torrent/src/TestConnectionPeers.java
+++ torrent/src/TestConnectionPeers.java
@@ -1,8 +1,16 @@
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.util.Formatter;
 import java.util.TreeMap;
 
@@ -18,34 +26,122 @@ public class TestConnectionPeers {
 		long size = file.length();
 		byte[] buffer = new byte[(int) size]; 
 		
-		is.read(buffer); 
-		System.out.println(getPos(buffer, 0, 4));
-		System.out.println(getPos(buffer, 4, 4));
-		System.out.println(getPos(buffer, 8, 4));
-		System.out.println(getPos(buffer, 12, 4));
-		System.out.println(getPos(buffer, 16, 4));
-		System.out.println(getPos(buffer, 16, 4));
-		
-		for (int i = 20; i < size; i += 6) { 
-			System.out.println(getIp(buffer, i));
-			System.out.println(getPort(buffer, i + 4));
-		}
-//		BencodingInputStream input = new BencodingInputStream(
-//				new FileInputStream("data1"));
-//		TreeMap tree = (TreeMap) input.readMap();		
-//		System.out.println(tree.keySet());
-//		TreeMap a = (TreeMap) tree.get("a");
-//		System.out.println(a.keySet());
-//		System.out.println(a.get("id").toString());
+//		is.read(buffer); 
+//		System.out.println(getPos(buffer, 0, 4));
+//		System.out.println(getPos(buffer, 4, 4));
+//		System.out.println(getPos(buffer, 8, 4));
+//		System.out.println(getPos(buffer, 12, 4));
+//		System.out.println(getPos(buffer, 16, 4));
+//		System.out.println(getPos(buffer, 16, 4));
 //		
-//		System.out.println(tree.get("q"));
+//		for (int i = 20; i < size; i += 6) { 
+//			System.out.println(getIp(buffer, i));
+//			System.out.println(getPort(buffer, i + 4));
+//		}
+		
+		BencodingInputStream input = new BencodingInputStream(
+				new FileInputStream("data1"));
+		TreeMap tree = (TreeMap) input.readMap();		
+		System.out.println(tree.keySet());
+		TreeMap a = (TreeMap) tree.get("a");
+		System.out.println(a.keySet());
+		byte[] id = (byte[]) a.get("id"); 
+		System.out.println(getPos(id, 0, id.length));
+		
+		byte[] q = (byte[]) tree.get("q");
+		System.out.println(getPos(q, 0, q.length));
+		
+		byte[] t = (byte[]) tree.get("t"); 
+		System.out.println(getPos(t, 0, t.length));
+		
+		byte[] v = (byte[]) tree.get("v"); 
+		System.out.println(getPos(v, 0, v.length));
+		
+		byte[] y = (byte[]) tree.get("y"); 
+		System.out.println(getPos(y, 0, y.length));
+		
+		input = new BencodingInputStream(new FileInputStream("data2"));
+		tree = (TreeMap) input.readMap();
+		System.out.println(tree.keySet());
+		TreeMap r = (TreeMap) tree.get("r"); 
+		System.out.println(r.keySet());
+		
+		id = (byte[]) r.get("id"); 
+		System.out.println("tree.r.id :\t\t" + getPos(id, 0, id.length));
+		
+		byte[] ip = (byte[]) r.get("ip");
+		System.out.println("tree.r.ip :\t\t" + getIp(ip, 0));
+		
+		t = (byte[]) tree.get("t"); 
+		System.out.println("tree.t :\t\t" + getPos(t, 0, t.length));
+		
+		v = (byte[]) tree.get("v"); 
+		System.out.println("tree.v :\t\t" + getPos(v, 0, v.length));
+		
+		y = (byte[]) tree.get("y"); 
+		System.out.println("tree.y :\t\t" + getPos(y, 0, y.length));
+		
+		input = new BencodingInputStream(new FileInputStream("data3"));
+		size = new File("data3").length();
+		byte[] data = new byte[(int) size]; 
+		input.read(data);
+		System.out.println(getPos(data, 0, data.length));
+		
+		
+		input = new BencodingInputStream(new FileInputStream("data4"));
+		size = new File("data4").length();
+		data = new byte[(int) size]; 
+		input.read(data);
+		System.out.println(getPos(data, 0, data.length));
+		
+		// try to make connection to this host
+//		int port = 59121;
+//		size = new File("data1").length();
+//		buffer = new byte[(int) size]; 
+//		input = new BencodingInputStream(
+//				new FileInputStream("data1"));	
+//		input.read(buffer);
+//		System.out.println(getPos(buffer, 0, (int) size));
+//		InetAddress ipAddr = InetAddress.getByAddress(new byte[] {(byte) 201, (byte) 227, (byte) 243, (byte) 174}); 
+//		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, ipAddr, 59121); 
+//		DatagramSocket socket = new DatagramSocket();
+//		
+//		socket.send(packet); 
+//		socket.receive(packet); 
+//		buffer = packet.getData();
+//		System.out.println();
+		
+		// try to make TCP connection with this host
+//		Socket s = null;
+//		try { 
+//			s = new Socket(ipAddr, port);
+//			System.out.println("already make connection");
+//			
+//			BufferedInputStream read =  new BufferedInputStream(s.getInputStream());
+//			BufferedOutputStream out = new BufferedOutputStream(s.getOutputStream());
+//			
+//			size = new File("bittorent-handshake").length();
+//			System.out.println(size);
+//			buffer = new byte[(int) size]; 
+//			input = new BencodingInputStream(
+//					new FileInputStream("bittorrent-handshake"));	
+//			input.read(buffer);
+//			
+//			out.write(buffer); 
+//			System.out.println("already sent.");
+//			int length = input.read(buffer); 
+//			System.out.println(length);
+//		} finally { 
+//			s.close();
+//		}
 	}
+	
 	
 	public static String getPos(byte[] a, int start, int size) { 
 		Formatter format = new Formatter();
 		
 		for (int i = start; i < start + size; i++) { 
-			format.format("%02x", a[i]);
+			format.format("%02x ", a[i]);
 		}
 		return format.toString();
 	}
@@ -53,7 +149,7 @@ public class TestConnectionPeers {
 	public static String getIp(byte[] a, int start) { 
 		String result = "";
 		for (int i = start; i < start + 4; i++) { 
-			String hex = getPos(a, i, 1); 
+			String hex = getPos(a, i, 1).trim(); 
 			int x = Integer.parseInt(hex, 16);
 			result += x  + ".";
 		}
--- torrent/src/Testing.java
+++ torrent/src/Testing.java
@@ -1,9 +1,6 @@
 import java.io.BufferedInputStream;
-import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.util.Arrays;
@@ -14,81 +11,80 @@ import java.util.TreeMap;
 import org.ardverk.coding.BencodingInputStream;
 import org.ardverk.coding.BencodingOutputStream;
 
-
 public class Testing {
-	public static void main(String[] args) throws Exception { 
-		BencodingInputStream input = new BencodingInputStream(new FileInputStream("file.torrent"));
-		BencodingOutputStream output = new BencodingOutputStream(new FileOutputStream("temp"));
+	public static void main(String[] args) throws Exception {
+		BencodingInputStream input = new BencodingInputStream(
+				new FileInputStream("file.torrent"));
+		BencodingOutputStream output = new BencodingOutputStream(
+				new FileOutputStream("temp"));
 		TreeMap tree = (TreeMap) input.readMap();
 		System.out.println(tree.keySet());
 		System.out.println(new String((byte[]) tree.get("announce")));
 		String mainURL = new String((byte[]) tree.get("announce"));
-//		String mainURL = "http://tracker.openbittorrent.com:80/announce";
+		// String mainURL = "http://tracker.openbittorrent.com:80/announce";
 		System.out.println(tree.get("encoded rate"));
 		List<Object> list = (List<Object>) tree.get("announce-list");
-		for (int i = 0; i < list.size(); i++) { 
-			System.out.println(new String((byte[]) ((List) list.get(i)).get(0)));
+		for (int i = 0; i < list.size(); i++) {
+			System.out
+					.println(new String((byte[]) ((List) list.get(i)).get(0)));
 		}
-		
+
 		TreeMap info = (TreeMap) tree.get("info");
 		System.out.println(info);
 		output.writeMap(info);
 		output.close();
 		System.out.println(new String((byte[]) info.get("name")));
-		
+
 		byte[] a = (byte[]) info.get("pieces");
-		System.out.println(Math.ceil(7957957/16384.0));
-		int length = (int) Math.ceil(7957957/16384.0); 
-		System.out.println(a.length/20);
+		System.out.println(Math.ceil(7957957 / 16384.0));
+		int length = (int) Math.ceil(7957957 / 16384.0);
+		System.out.println(a.length / 20);
 		MessageDigest sha1 = MessageDigest.getInstance("SHA1");
 		for (int i = 0; i < length; i++) {
-			byte[] temp = Arrays.copyOfRange(a, i * 20, (i+1) * 20);
+			byte[] temp = Arrays.copyOfRange(a, i * 20, (i + 1) * 20);
 		}
-//		DatagramPacket connection = new DatagramPacket(new byte[126], 126, udp, 80);
-		
+		// DatagramPacket connection = new DatagramPacket(new byte[126], 126,
+		// udp, 80);
+
 		String infoHash = calculateHash("temp");
 		System.out.println(infoHash);
 		String peerId = calculateHash("temp-id");
 		System.out.println(peerId);
 		int port = 6700;
-		int uploaded = 0; 
-		int downloaded = 0; 
-		int left = 7957957; 
-		
-		String request = 
-				"?info_hash=" + infoHash
-				+ "&peer_id=" + peerId
-				+ "&port=" + port
-				+ "&downloaded="+ downloaded
-				+ "&left=" + left
-				+ "&event=started";
-		
+		int uploaded = 0;
+		int downloaded = 0;
+		int left = 7957957;
+
+		String request = "?info_hash=" + infoHash + "&peer_id=" + peerId
+				+ "&port=" + port + "&downloaded=" + downloaded + "&left="
+				+ left + "&event=started";
+
 		System.out.println(mainURL);
 		System.out.println(request);
 	}
-	
-	public static String calculateHash(String fileName) throws Exception{
 
-			MessageDigest algorithm = MessageDigest.getInstance("SHA1");
-	        FileInputStream     fis = new FileInputStream(fileName);
-	        BufferedInputStream bis = new BufferedInputStream(fis);
-	        DigestInputStream   dis = new DigestInputStream(bis, algorithm);
+	public static String calculateHash(String fileName) throws Exception {
 
-	        // read the file and update the hash calculation
-	        while (dis.read() != -1);
+		MessageDigest algorithm = MessageDigest.getInstance("SHA1");
+		FileInputStream fis = new FileInputStream(fileName);
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		DigestInputStream dis = new DigestInputStream(bis, algorithm);
 
-	        // get the hash value as byte array
-	        byte[] hash = algorithm.digest();
+		// read the file and update the hash calculation
+		while (dis.read() != -1)
+			;
 
-	        return byteArray2Hex(hash);
-	    }
+		// get the hash value as byte array
+		byte[] hash = algorithm.digest();
 
-	
+		return byteArray2Hex(hash);
+	}
+
 	private static String byteArray2Hex(byte[] hash) {
-        Formatter formatter = new Formatter();
-        for (byte b : hash) {
-            formatter.format("%02x", b);
-        }
-        return formatter.toString();
-    }
+		Formatter formatter = new Formatter();
+		for (byte b : hash) {
+			formatter.format("%02x", b);
+		}
+		return formatter.toString();
+	}
 }
