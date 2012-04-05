adb3b3f7253b53b520d7f84b3b8cabd283b54cf5
Do Nguyen <ntd172@gmail.com>
2012-04-04 21:56:52 -0700

    get first reponse back from tracker

--- torrent/src/HttpTest.java
+++ torrent/src/HttpTest.java
@@ -7,17 +7,27 @@ import java.util.Formatter;
 class HttpTest { 
 	public static void main(String[] args) throws IOException {
 		
-		// connect input
-		byte[] buffer = new byte[100];
-		int length = 100;
-		InetAddress ip = InetAddress.getByAddress(new byte[] {(byte) 95, (byte) 215, (byte) 62, (byte) 26});
+		// HOST = 192.168.0.11
+		// TRACKER = 95.215.62.5		port = http (80)
+		InetAddress ip = InetAddress.getByAddress(new byte[] {(byte) 95, (byte) 215, (byte) 62, (byte) 5});
+		
+		// connection request
+		// data : 00 00 04 17 27 10 19 80 00 00 00 00 c6 a2 92 eb
+		// convert to byte[] array
+		byte[] buffer = new byte[] {0x00, 0x00, 0x04, 0x17, 0x27, 0x10, 0x19, (byte) 0x80, 0x00, 0x00, 0x00, 0x00, (byte) 0xc6, (byte) 0xa2, (byte) 0x92, (byte) 0xeb};
+		int length = buffer.length;
+		
+		// DatagramPacket is used to send and received UDP packet 
 		DatagramPacket packet = new DatagramPacket(buffer, length, ip, 80);
 		DatagramSocket socket = new DatagramSocket();
 		socket.send(packet);
 		
-		socket.connect(ip, 80);
+		System.out.println("Already sent");
+		
 		socket.receive(packet);
+		
 		System.out.println(byteArray2Hex(packet.getData()));
+		System.out.println("Connect response");
 		socket.close();
 		
     }
