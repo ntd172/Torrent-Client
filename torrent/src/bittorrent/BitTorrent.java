fae7743111c7b2f9bd62b2c7d780027c12b386a8
Do Nguyen <ntd172@gmail.com>
2012-04-07 15:33:32 -0700

    organize

--- torrent/src/bittorrent/BitTorrent.java
+++ torrent/src/bittorrent/BitTorrent.java
@@ -1,15 +1,11 @@
 package bittorrent;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.Arrays;
 
 import message.BitTorrentPacket;
 import message.HandShake;
